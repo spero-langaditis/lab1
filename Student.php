276589b4139977805f3e53aaaee8e07aeddfb421
spero <slangaditis94@hotmail.com>
2015-09-11 10:06:21 -0700

    commented code

--- Student.php
+++ Student.php
@@ -13,22 +13,26 @@
  */
 class Student {
 
+    // sets up a student
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
-
+    
+    //adds the email to the student
     function
     add_email($which, $address) {
         $this->emails[$which] = $address;
     }
-
+    
+    //adds the grade to the student
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
-
+    
+    //calculates the average for all the students grades
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -37,6 +41,7 @@ class Student {
                 count($this->grades);
     }
 
+    //puts the student into a string to be displayed
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= '(' . $this->average() . ")\n";
